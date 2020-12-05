0d250d2d44137424646a4118fc3bd8ffeac36920
Joshua C Agar <jca318@lehigh.edu>
2020-12-04 22:12:53 -0500

    add image

---
+++ AE_tutorial/viz.py
@@ -0,0 +1,96 @@
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+
+
+def layout_fig(graph, mod=None):
+
+    """
+    function
+
+    :param graph: number of axes to make
+    :type graph: int
+    :param mod: sets the number of figures per row
+    :type mod: int (, optional)
+    :return: fig:
+                handel to figure being created
+             axes:
+                numpy array of axes that are created
+    :rtype: fig:
+                matplotlib figure
+            axes:
+                numpy array
+    """
+
+    # Sets the layout of graphs in matplotlib in a pretty way based on the number of plots
+    if mod is None:
+        # Selects the number of columns to have in the graph
+        if graph < 3:
+            mod = 2
+        elif graph < 5:
+            mod = 3
+        elif graph < 10:
+            mod = 4
+        elif graph < 17:
+            mod = 5
+        elif graph < 26:
+            mod = 6
+        elif graph < 37:
+            mod = 7
+
+    # builds the figure based on the number of graphs and selected number of columns
+    fig, axes = plt.subplots(graph // mod + (graph % mod > 0), mod,
+                             figsize=(3 * mod, 3 * (graph // mod + (graph % mod > 0))))
+
+    # deletes extra unneeded axes
+    axes = axes.reshape(-1)
+    for i in range(axes.shape[0]):
+        if i + 1 > graph:
+            fig.delaxes(axes[i])
+
+    return (fig, axes)
+
+
+def embedding_maps(data, image, colorbar_shown=True,
+                   c_lim=None, mod=None,
+                   title=None):
+
+    """
+
+    :param data: data need to be showed in image format
+    :type data: array
+    :param image: the output shape of the image
+    :type image: array
+    :param colorbar_shown: whether to show the color bar on the left of image
+    :type colorbar_shown: boolean
+    :param c_lim: Sets the scales of colorbar
+    :type c_lim: list
+    :param mod: set the number of image for each line
+    :type mod: int
+    :param title: set the title of figure
+    :type title: string
+    :return: handel to figure being created
+    :rtype: matplotlib figure
+    """
+    fig, ax = layout_fig(data.shape[1], mod)
+
+    for i, ax in enumerate(ax):
+        if i < data.shape[1]:
+            im = ax.imshow(data[:, i].reshape(image.shape[0], image.shape[1]))
+            ax.set_xticklabels('')
+            ax.set_yticklabels('')
+
+            # adds the colorbar
+            if colorbar_shown == True:
+                divider = make_axes_locatable(ax)
+                cax = divider.append_axes('right', size='10%', pad=0.05)
+                cbar = plt.colorbar(im, cax=cax, format='%.1e')
+
+                # Sets the scales
+                if c_lim is not None:
+                    im.set_clim(c_lim)
+
+    if title is not None:
+        # Adds title to the figure
+        fig.suptitle(title, fontsize=16,
+                     y=1, horizontalalignment='center')
+
+    fig.tight_layout()
